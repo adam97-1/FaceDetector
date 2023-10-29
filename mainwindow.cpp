e77d0b47c7e4b0f69f9bbbd09e3281d93bffe9c5
Adam Drożdż <adamdrozdz971@gmail.com>
2023-10-29 21:09:44 +0100

    Dodanie wytrenowanego modelu.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,10 +10,10 @@ MainWindow::MainWindow(QWidget *parent)
 
 
 //     Load classifiers from "opencv/data/haarcascades" directory
-    nestedCascade.load("./haarcascades/haarcascade_eye.xml") ;
+//    nestedCascade.load("./haarcascades/haarcascade_eye.xml") ;
 
     // Change path before execution
-    cascade.load("./haarcascades/haarcascade_frontalface_alt.xml") ;
+    cascade.load("./cascade/cascade.xml") ;
 
 
     timer->start(1);
@@ -56,36 +56,26 @@ void MainWindow::detectAndDraw(cv::Mat &img, cv::CascadeClassifier &cascade, cv:
         std::vector<cv::Rect> nestedObjects;
         cv::Point center;
         cv::Scalar color = cv::Scalar(255, 0, 0); // Color for Drawing tool
-        int radius;
 
-        double aspect_ratio = (double)r.width/r.height;
-        if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
-        {
-            center.x = cvRound((r.x + r.width*0.5)*scale);
-            center.y = cvRound((r.y + r.height*0.5)*scale);
-            radius = cvRound((r.width + r.height)*0.25*scale);
-            circle( img, center, radius, color, 3, 8, 0 );
-        }
-        else
-            rectangle( img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)),
+        rectangle( img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)),
                       cvPoint(cvRound((r.x + r.width-1)*scale),
                               cvRound((r.y + r.height-1)*scale)), color, 3, 8, 0);
-        if( nestedCascade.empty() )
-            continue;
-        smallImgROI = smallImg( r );
+//        if( nestedCascade.empty() )
+//            continue;
+//        smallImgROI = smallImg( r );
 
-        // Detection of eyes in the input image
-        nestedCascade.detectMultiScale( smallImgROI, nestedObjects, 1.1, 2,
-                                       0|cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30) );
+//        // Detection of eyes in the input image
+//        nestedCascade.detectMultiScale( smallImgROI, nestedObjects, 1.1, 2,
+//                                       0|cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30) );
 
-        // Draw circles around eyes
-        for ( size_t j = 0; j < nestedObjects.size(); j++ )
-        {
-            cv::Rect nr = nestedObjects[j];
-            center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale);
-            center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale);
-            radius = cvRound((nr.width + nr.height)*0.25*scale);
-            circle( img, center, radius, color, 3, 8, 0 );
-        }
+//        // Draw circles around eyes
+//        for ( size_t j = 0; j < nestedObjects.size(); j++ )
+//        {
+//            cv::Rect nr = nestedObjects[j];
+//            center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale);
+//            center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale);
+//            radius = cvRound((nr.width + nr.height)*0.25*scale);
+//            circle( img, center, radius, color, 3, 8, 0 );
+//        }
     }
 }
